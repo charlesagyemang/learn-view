98429181b66bbed83cf8c8db3bd324e815764ae9
charlesagymang <micnkru@gmail.com>
2020-03-13 09:33:55 +0000

    Bind attributes to button'

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let app = new Vue({
     image: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Socks-White-LB1_1024x1024.jpg?v=1561393817',
     image2: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Unisex-CrewSocks-Navy-LB2_864x864.jpg?v=1561506248',
     inventory: 90,
+    inStock: true,
     details: [
       "80% Cotton",
       "20% Polyester",
@@ -17,12 +18,12 @@ let app = new Vue({
     variants: [
       {
         variantID: 2234,
-        variantColor: 'White',
+        variantColor: 'green',
         variantImage: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Socks-White-LB1_1024x1024.jpg?v=1561393817',
       },
       {
         variantID: 2235,
-        variantColor: 'Blue-Black',
+        variantColor: 'blue',
         variantImage: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Unisex-CrewSocks-Navy-LB2_864x864.jpg?v=1561506248',
       },
 
