6d471d814277a89b66b738487307f201fd3bfaf5
charlesagymang <micnkru@gmail.com>
2020-03-13 07:55:20 +0000

    feat:Level One Learn View

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Boots',
+    google_link: "https://google.com",
+    image: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Socks-White-LB1_1024x1024.jpg?v=1561393817',
+    image2: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Unisex-CrewSocks-Navy-LB2_864x864.jpg?v=1561506248'
+  }
+});
