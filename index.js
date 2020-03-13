748825e029fba468ed192e04d01cf20102543e91
charlesagymang <micnkru@gmail.com>
2020-03-13 08:20:44 +0000

    Feat: simple ordered list oimplementatrion

--- index.js
+++ index.js
@@ -9,5 +9,10 @@ let app = new Vue({
     image: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Socks-White-LB1_1024x1024.jpg?v=1561393817',
     image2: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Unisex-CrewSocks-Navy-LB2_864x864.jpg?v=1561506248',
     inventory: 90,
+    details: [
+      "80% Cotton",
+      "20% Polyester",
+      "General-neutral",
+    ],
   }
 });
