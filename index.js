a2b00cd48c7750aec542ec1a8feb41b878a41a27
charlesagymang <micnkru@gmail.com>
2020-03-13 08:05:35 +0000

    if else imp

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let app = new Vue({
   el: '#app',
   data: {
     product: 'Boots',
-    google_link: "https://google.com",
+    googleLink: "https://google.com",
     image: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Socks-White-LB1_1024x1024.jpg?v=1561393817',
     image2: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Unisex-CrewSocks-Navy-LB2_864x864.jpg?v=1561506248',
-    inStock: false,
+    inventory: 100,
   }
 });
