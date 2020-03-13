7508870f30cd7a925660e37e808a92f59361dce4
charlesagymang <micnkru@gmail.com>
2020-03-13 08:15:25 +0000

    v-show over v-if

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ let app = new Vue({
     googleLink: "https://google.com",
     image: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Socks-White-LB1_1024x1024.jpg?v=1561393817',
     image2: 'https://cdn.shopify.com/s/files/1/0052/7237/1293/products/1024x1024-Unisex-CrewSocks-Navy-LB2_864x864.jpg?v=1561506248',
-    inventory: 100,
+    inventory: 90,
   }
 });
